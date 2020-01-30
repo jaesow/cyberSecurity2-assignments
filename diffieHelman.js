f7a2792b59d6aa017648537f1aa737a65b5e8e4f
Jenaba Sow <jenabasow@gmail.com>
2020-01-30 01:07:52 -0500

    Update diffieHelman.js

--- diffieHelman.js
+++ diffieHelman.js
@@ -7,3 +7,11 @@ const assert = require('assert')
 
 const trialOne = createDiffHel(1024)
 const trialOneKey = trialOne.generateKeys()
+
+// derive bob's keys from alice's key generation parameters
+const trialTwo = createDiffHel(trialOne.prime(), trialOne.generator())
+const trialTwoKey = bob.generateKeys()
+
+// compute alice and bob's shared secret
+const trialOneSecret = trialOne.computeSecret(trialTwoKey)
+const trialTwoSecret = trialTwo.computeSecret(trialOneKey)
