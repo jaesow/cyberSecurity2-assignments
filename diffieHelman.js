655614ca6d7cdbd1f4ae939df1919fc22c9db0e8
Jenaba Sow <jenabasow@gmail.com>
2020-01-30 01:15:06 -0500

    Update diffieHelman.js

--- diffieHelman.js
+++ diffieHelman.js
@@ -8,10 +8,18 @@ const assert = require('assert')
 const trialOne = createDiffHel(1024)
 const trialOneKey = trialOne.generateKeys()
 
-// derive bob's keys from alice's key generation parameters
+
 const trialTwo = createDiffHel(trialOne.prime(), trialOne.generator())
 const trialTwoKey = bob.generateKeys()
 
-// compute alice and bob's shared secret
+
 const trialOneSecret = trialOne.computeSecret(trialTwoKey)
 const trialTwoSecret = trialTwo.computeSecret(trialOneKey)
+
+// symmetric key used for encrypted communication
+try {
+  assert.strictEqual(trialOneSecret.toString('hex'), trialTwoSecret.toString('hex'))
+  console.log('They computed the same secret, which means they are able to symmetrically encrypt messages :)')
+} catch (e) {
+  console.error('They computed different secrets :(')
+}
